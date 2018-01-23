d242999f89797562ef87798886ce03c4fa10631d
rpiccoline1 <rpiccoline@hotmail.com>
2018-01-23 17:30:50 -0500

    updated.  still issues with timer

--- assets/javascript/app1.js
+++ assets/javascript/app1.js
@@ -18,22 +18,42 @@ var questions = [{
     question: "What is the name of the lead character from Earthbound?",
     choices: ["Ninten", "Ness", "Jeff", "Paula"],
     validAnswer: 1
+  }, {
+    question: "Nintendo broke off an agreement with Sony while developing project which led to the creation of the Sony Playstation?",
+    choices: ["True", "False"],
+    validAnswer: 0
+  }, {
+    question: "How rich is Valve?",
+    choices: ["They're broke", "They have enough money to end world hunger", "eh, I've seen richer"],
+    validAnswer: 1
+  }, {
+    question: "Where did Shigeru Miyamoto's inspiration come from when creating The Legend of Zelda",
+    choices: ["movies", "fairytales", "exploring as a child", "drugs"],
+    validAnswer: 2
+  }, {
+    question: "What was the term coined for Sonic the Hedgehog's game engine to explain the high speeds found in the game?",
+    choices: ["fast processing", "quick processing", "blitz processing", "blast processing"],
+    validAnswer: 3
+  }, {
+    question: "Is Pokemon morally messed up?",
+    choices: ["yes", "no, nothing wrong with sending unsupervised children out into the world to make animals fight."],
+    validAnswer: 1
 }];
 
-var count = 60;
+
 var currentquestion = 0;
 var correctAnswers = 0;
 
 function gameSetup() {
 $('#question').html(parseInt(currentquestion) + 1 + ". " + questions[currentquestion].question);
 var options = questions[currentquestion].choices;
-var formHtml = '';
+var layout = '';
 for (var i = 0; i < options.length; i++) {
-  formHtml += '<div><input type="radio" name="option" value="' + i + '" id="option' + i + '"><label for="option' + i + '">' +
+  layout += '<div><input type="radio" name="option" value="' + i + '" id="option' + i + '"><label for="option' + i + '">' +
     questions[currentquestion].choices[i] + '</label></div><br/>';
 }
 
-$('#form').html(formHtml);
+$('#form').html(layout);
 $("#option0").prop('checked', true);
 };
 
@@ -45,12 +65,25 @@ if ($("input[name=option]:checked").val() == questions[currentquestion].validAns
 
 $(document).ready(function() {
 
+var count = 60;
+
 $(".jumbotron").hide();
 $('#start').click(function() {
   $(".jumbotron").fadeIn();
   $(this).hide();
- 
-  });
+
+clock = setInterval(timer, 1000);
+
+function timer() {
+  count--;
+  if (count <= 0) {
+    clearInterval(clock);
+    return;
+  }
+  $("#Timer").html("Time Left: " + count);
+}
+
+});
 
 gameSetup();
 
@@ -65,7 +98,7 @@ $("#next").click(function() {
       $('#next').html("Submit");
       $('#next').click(function() {
         $(".jumbotron").hide();
-        $("#results").html("You correctly answered " + correctAnswers + " out of " + currentquestion + " questions! ").show();
+        $("#results").html("You correctly answered " + correctAnswers + " out of " + currentquestion + " ! ").show();
       });
     };
   };
